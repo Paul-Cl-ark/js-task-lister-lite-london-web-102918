0836cdba855fc2c87e5ac50433d7888337e9dc9d
Paul-Cl-ark <clark.paul123@gmail.com>
2018-12-17 11:24:34 +0000

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,30 @@
+// View
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
-});
+  // const taskList = new TaskList()
+
+  const formEl = document.querySelector('#create-task-form')
+  formEl.addEventListener("submit", addToDo)
+  const listEl = document.querySelector('#tasks')
+  const inputEl = document.querySelector('#new-task-description')
+
+  function addToDo (event) {
+    event.preventDefault()
+
+    // create the li
+    let newToDo = document.createElement('li')
+    let newDeleteBut = document.createElement('button')
+
+    // use submitted text to put inside the li
+    newToDo.innerHTML = inputEl.value
+    newDeleteBut.innerHTML = "x"
+    // append the new ToDo to the list
+    listEl.appendChild(newToDo)
+    newToDo.appendChild(newDeleteBut)
+    newDeleteBut.addEventListener('click', deleteToDo)
+  }
+
+  function deleteToDo (event) {
+    event.target.parentNode.remove()
+  }
+})
